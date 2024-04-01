6cfc1cf79ca4be4a8eb3a165dbfe176b7ef3ea5a
Yuka <90426808+yuka-with-data@users.noreply.github.com>
2024-04-01 12:42:14 -0700

    Edit text and added privacy statement

--- Home.py
+++ Home.py
@@ -7,19 +7,20 @@ st.set_page_config(
 
 st.write("# Welcome to Music dataGalax! 👋")
 
-st.sidebar.success("Select an analysis from the sidebar to begin exploring music data.")
+with st.sidebar:
+    st.success("☝️Select an analysis from the sidebar to begin exploring music data.")
 
 st.markdown(
     """
     Welcome to **Music dataGalax**, your go-to app for exploring and analyzing music data! 
     Dive into the world of music through data-driven insights and interactive visualizations. 
-    Whether you're a music enthusiast, data analyst, or industry professional, our app offers a 
+    Whether you're a music enthusiast or data analyst, our app offers a 
     variety of analyses tailored to uncover the stories behind the songs, artists, and trends 
     shaping the music world.
 
     ## Features
     - **Top Chart Analysis:** Dive into the most popular tracks and artists dominating the charts.
-    - **Track Recommendation:** Discover new music based on your taste and preferences.
+    - **Track Recommendation:** Discover new music based on your favorite track.
     - **Popularity Check:** Analyze the popularity trends of songs and artists over time.
     - **Track Comparison:** Compare the attributes of different tracks to see what makes a hit.
     - **Era Comparison:** Explore how musical trends and preferences have evolved across different eras.
@@ -36,7 +37,13 @@ st.markdown(
     Streamlit is an open-source app framework perfect for building interactive and shareable web apps for data science and machine learning projects.
     - Get started with Streamlit at [streamlit.io](https://streamlit.io)
     - Jump into Streamlit [documentation](https://docs.streamlit.io)
+
+    #### Privacy Statement
+    No personal data is logged by this app. No log-in required.
 """
 )
 
 st.image('images/dataGalax.png', use_column_width=True, caption="Explore the rhythm of data.")
+
+st.divider()
+st.caption("🛑Disclaimer: This application is designed for educational and entertainment purposes only. The data used in this app is sourced from public APIs and may not be entirely accurate or up-to-date. The analysis and visualizations provided should not be considered professional advice, and the creators of this app are not responsible for any decisions made based on the information provided. Please note that this app is not affiliated with Spotify or any other third-party services. Use this app responsibly and be aware of the terms and conditions of the Spotify API. Enjoy exploring the data responsibly!")
