49935586cc369d319055e132f6640736629dbaef
Y M <90426808+yuka-with-data@users.noreply.github.com>
2024-04-16 11:28:22 -0700

    Editted and Modified main page

--- Home.py
+++ Home.py
@@ -1,22 +1,31 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Music dataGalax",
+    page_title="rhythmGalax",
     page_icon="👋",
 )
 
-st.write("# Welcome to Music dataGalax! 👋")
+st.write("# Welcome to rhythmGalax! 👋")
 
 with st.sidebar:
     st.success("☝️Select an analysis from the sidebar to begin exploring music data.")
+    st.divider()
+    st.markdown("""
+    🌟 **Now Live:**
+    - **Album Analysis:** 
+    - **Fine-Tuned Track Recommendations:**
+                
+    🚧 **In Development:**
+    - **Artist Analysis**
+    - **Soothing Music Chatbot**
+        """)
 
 st.markdown(
     """
-    Welcome to **Music dataGalax**, your go-to app for exploring and analyzing music data! 
+    Welcome to **rhythmGalax**, your go-to app for exploring and analyzing music data! 
     Dive into the world of music through data-driven insights and interactive visualizations. 
-    Whether you're a music enthusiast or data analyst, our app offers a 
-    variety of analyses tailored to uncover the stories behind the songs, artists, and trends 
-    shaping the music world.
+    Whether you're a music enthusiast or a data analyst, this app offers a variety of analyses tailored to uncover the data stories behind the songs, artists, and trends shaping the music world. 
+    Enhance your experience by listening to your chosen tracks or albums directly within the app, as you explore the rich data each piece of music holds.
 
     ## Features
     - **Top Chart Analysis:** Dive into the most popular tracks and artists dominating the charts.
@@ -24,13 +33,15 @@ st.markdown(
     - **Popularity Check:** Analyze the popularity trends of songs and artists over time.
     - **Track Comparison:** Compare the attributes of different tracks to see what makes a hit.
     - **Era Comparison:** Explore how musical trends and preferences have evolved across different eras.
+    - **Album Analysis:** Examine entire albums to understand the collective mood and technical attributes. 
+    - **Fine-Tuned Recommendation:** Harness advanced controls to tailer music recommendations to your specific preferences. 
 
     Ready to uncover the secrets of the music industry through data?  
     **👈 Select an analysis from the sidebar** and let the journey begin!
 
     ### Want to learn more about music data analysis?
     Whether you're new to the field or looking to expand your knowledge, check out these resources:
-    - Delve into our [documentation](https://docs.yourapp.com) for detailed guides on using the app.
+    - Delve into the [README doc](https://github.com/yuka-with-data/Spotify-Data-App-Public) for detailed guides on using the app.
     - Join the conversation in our community forum (Coming Soon)
 
     ### Inspired to create your own data project?
@@ -46,4 +57,4 @@ st.markdown(
 st.image('images/dataGalax.png', use_column_width=True, caption="Explore the rhythm of data.")
 
 st.divider()
-st.caption("🛑Disclaimer: This application is designed for educational and entertainment purposes only. The data used in this app is sourced from public APIs and may not be entirely accurate or up-to-date. The analysis and visualizations provided should not be considered professional advice, and the creators of this app are not responsible for any decisions made based on the information provided. Please note that this app is not affiliated with Spotify or any other third-party services. Use this app responsibly and be aware of the terms and conditions of the Spotify API. Enjoy exploring the data responsibly!")
+st.caption("🛑Disclaimer: This application is designed for educational and entertainment purposes only. The data used in this app is sourced from public APIs and may not be entirely accurate or up-to-date. Embedded Spotify players are used under the terms of Spotify's embedding policies to enhance user experience by providing direct access to playlists and tracks. The analysis and visualizations provided should not be considered professional advice, and the creators of this app are not responsible for any decisions made based on the information provided. Please note that this app is not affiliated with Spotify or any other third-party services. Use this app responsibly and be aware of the terms and conditions of the Spotify API and the implications of embedded content. Enjoy exploring the data responsibly!")
