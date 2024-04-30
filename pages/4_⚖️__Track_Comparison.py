8abbe479083f0a3159f4e08bc108a402a48915a6
Y M <90426808+yuka-with-data@users.noreply.github.com>
2024-04-30 10:56:02 -0700

    Moved spotify client oath and data retriever to the central file

--- pages/4_⚖️__Track_Comparison.py
+++ pages/4_⚖️__Track_Comparison.py
@@ -21,121 +21,15 @@ from typing import Optional, Dict, Tuple, List, Union
 st.set_page_config(page_title="Track-To-Chart Comparison", 
                    page_icon="⚖️")
 
-# Inner function starts
-@st.cache_data(ttl=86400, show_spinner=False)
-def _fetch_playlist_data(_sp, playlist_id:str) -> Tuple[pd.Series, pd.DataFrame]:
-    try:
-        tracks_data = []
-        results = _sp.playlist_tracks(playlist_id)
-        # When no tracks found
-        total_tracks = len(results['items'])
-        if total_tracks == 0:
-            st.error('No tracks found in the playlist')
-            return pd.Series(), pd.DataFrame()
-        
-        # Initialize a progress bar in the app
-        progress_bar = st.progress(0)
-
-        for index, item in enumerate(results['items']):
-            # Update progress bar based on the number of tracks proceeded
-            percent_complete = int((index + 1) / total_tracks * 100)
-            progress_bar.progress(percent_complete, text="🛰️Fetching The Most Up-To-Date Chart Data. Please Wait.")
-
-            # Track
-            track = item['track']
-            if not track or not track.get('id'):
-                continue # skip tracks with missing info 
-
-            # Artist
-            artist = track['artists'][0] if track['artists'] else None
-            if not artist:
-                continue # skip tracks with missing artist info
-
-            track_id = track['id']
-            genres = _sp.artist(track['artists'][0]['id'])['genres']
-
-            track_info = {
-                'artist_name': track['artists'][0]['name'],
-                'track_name': track['name'],
-                'is_explicit': track['explicit'],
-                'album_release_date': track['album']['release_date'],
-                'genres': ', '.join(genres)  # Join genres list into a string
-            }
-
-            # Fetch audio features
-            audio_features = _sp.audio_features(track_id)[0]
-            if not audio_features:
-                continue # skip tracks with missing audio features
-            
-            track_info.update({
-                'danceability': audio_features['danceability'],
-                'valence': audio_features['valence'],
-                'energy': audio_features['energy'],
-                'loudness': audio_features['loudness'],
-                'acousticness': audio_features['acousticness'],
-                'instrumentalness': audio_features['instrumentalness'],
-                'liveness': audio_features['liveness'],
-                'speechiness': audio_features['speechiness'],
-                'key': audio_features['key'],
-                'tempo': audio_features['tempo'],
-                'mode': audio_features['mode'],
-                'duration_ms': audio_features['duration_ms'],
-                'time_signature': audio_features['time_signature']
-            })
-            # Fetch Popularity
-            search_query = f"{track['name']} {track['artists'][0]['name']}"
-            popularity_result = _sp.search(q=search_query, type='track', limit=1)
-            print(f"Popularity Result: {popularity_result}")
-            if popularity_result['tracks']['items']:
-                popularity = popularity_result['tracks']['items'][0]['popularity']
-            else:
-                popularity = None
-            track_info['popularity'] = popularity
-
-            tracks_data.append(track_info)
-        
-        # Progress bar complete
-        progress_bar.progress(100)
-        # Success msg with a placeholder
-        success_placeholder = st.empty()
-        success_placeholder.success(f"Retrieved {total_tracks} Top Tracks from the playlist!", icon="✅")
-        # Display the msg for 2 seconds
-        time.sleep(2)
-        
-        success_placeholder.empty()
-        progress_bar.empty()
-
-        # Save the tracks data to a DataFrame
-        df = pd.DataFrame(tracks_data)
-        # Calculate mean values for each attribute
-        att_list = ['danceability', 'valence', 'energy', 'acousticness', 'instrumentalness', 'liveness', 'speechiness']
-        selected_atts = df[att_list].mean()
-
-        return selected_atts, df
-
-    except Exception as e:
-        print(f"Error: {e}")
-        st.error("Failed to retrieve the latest Top 50 Tracks data. Please try again later.")
-        return pd.Series(), pd.DataFrame()
-
-def retrieve_latest_data(_sp, playlist_id: str)-> Tuple[pd.Series, pd.DataFrame]:
-    try:
-        selected_atts, df = _fetch_playlist_data(_sp, playlist_id)
-        if df.empty:
-            st.error("Failed to retrieve the latest Top 50 Tracks data. Please try again later.")
-            return pd.Series(), pd.DataFrame()
-        return selected_atts, df
-    
-    except Exception as e:
-        st.error(f"An error occured: {str(e)}")
-        return pd.Series(), pd.DataFrame()
+# Import data_galaxy after Page Config
+from data_galaxy import init_spotify_client, retrieve_playlist_data
 
 # Class
 class SpotifyAnalyzer:
     def __init__(self, sp, playlist_id:str) -> None:
         self.sp = sp
         # Load Playlist Data
-        self.mean_values_top_50, self.df_top_50 = retrieve_latest_data(self.sp, playlist_id)
+        self.mean_values_top_50, self.df_top_50 = retrieve_playlist_data(self.sp, playlist_id)
 
     #@st.cache_data
     def get_spotify_data(_self, artist:str, track:str) -> Tuple[Optional[Dict[str, float]], Optional[str]]:
@@ -750,13 +644,8 @@ class SpotifyAnalyzer:
             st.error("An error occurred during analysis. Please try again.")
             # st.exception(e)
 
+
 # Initialize the Spotify client
-def init_spotify_client():
-    client_id = st.secrets["SPOTIFY_CLIENT_ID"]  #config('SPOTIFY_CLIENT_ID')
-    client_secret = st.secrets["SPOTIFY_CLIENT_SECRET"] # config('SPOTIFY_CLIENT_SECRET')
-    credential_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-    sp = spotipy.Spotify(auth_manager=credential_manager)
-    return sp
 sp = init_spotify_client()
 
 
