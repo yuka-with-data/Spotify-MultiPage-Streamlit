f5074e66e087875a0c730058f07359dcb6983ad1
Y M <90426808+yuka-with-data@users.noreply.github.com>
2024-05-22 14:41:57 -0700

    Fixed genre tag

--- pages/8_👯_Artist_Analysis.py
+++ pages/8_👯_Artist_Analysis.py
@@ -220,14 +220,23 @@ class SpotifyAnalyzer:
             st.header('Artist Tracks Table')
             st.dataframe(self.df_artist)
 
+            # Display Artist Genres
             st.header('Artist Genre')
             st.text("This section displays the top genres associated with the artist's tracks.")
-            artist_genres = set(self.df_artist['genres'])
+            # Extract and process genres
+            def extract_genres(df):
+                genres = set()
+                for genre_list in df['genres']:
+                    individual_genres = genre_list.split(',')
+                    for genre in individual_genres:
+                        genres.add(genre.strip())
+                return genres
+            artist_genres = extract_genres(self.df_artist)
             artist_genres_list = list(artist_genres)
-            badges_html = " ".join([f"<span style='background-color:rgba(26, 12, 135, 0.9); color:#ffffff; padding:5px; border-radius:5px; margin:2px; display:inline-block;'>{genre}</span>" for genre in artist_genres_list])
-            st.write(f"<div>{badges_html}</div>", unsafe_allow_html=True)
-
-            #st.write(f"<p style='font-size:24px; font-family:Roboto;'>Artist Genres: {artist_genres_str}</p>", unsafe_allow_html=True)
+            # Create HTML for genres as badges with the specified background color
+            badges_html = "".join([f"<span style='background-color:rgba(26, 12, 135, 0.9); color:#ffffff; padding:5px; border-radius:5px; margin:2px; display:inline-block;'>{genre}</span>" for genre in artist_genres_list])
+            # Display the genres using HTML
+            st.write(f"<div style='display: flex; flex-wrap: wrap; gap: 10px;'>{badges_html}</div>", unsafe_allow_html=True)
 
             # Create a Radar Chart
             st.header('Attributes Radar Chart')
